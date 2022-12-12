e65d65d1718aadd698fb838647b7d6757b731358
Vitaliy Zaytsev <teh.kroleg@gmail.com>
2022-12-12 13:07:48 +0300

    fixed aws-creds fn

--- zsh/custom/awscreds.zsh
+++ zsh/custom/awscreds.zsh
@@ -1,6 +1,7 @@
 function aws-creds() {
   PROFILE=$(aws configure list-profiles | fzf)
-  aws sso login --profile=$PROFILE && eval $(aws-sso-creds --profile=$PROFILE export)
+  aws sso login --profile=$PROFILE
+  # && eval $(aws-sso-creds --profile=$PROFILE export)
   export AWS_PROFILE=$PROFILE
-  kubectx $PROFILE
+  kubectl config use-context $PROFILE
 }
