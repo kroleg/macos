845c9118bc28c60d6f25eba6d624893344c3daf7
Vitaliy Zaytsev <teh.kroleg@gmail.com>
2023-05-17 13:44:21 +0300

    added cdc alias to quickly change folder to one of code/stashaway subfolders

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,2 +1,11 @@
 alias tp="telepresence"
 alias k="kubectl"
+
+cdc() {
+  cd "$HOME/Code/stashaway/$1"
+}
+_cdc () {
+  ((CURRENT == 2)) &&
+  _files -/ -W /$HOME/Code/stashaway -/ -/
+}
+compdef _cdc cdc
