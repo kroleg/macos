0f289315ee9b01f62b8766fceb11bfac570ec642
Vitaliy Zaytsev <teh.kroleg@gmail.com>
2025-01-21 17:09:22 +0300

    style fzf
    - hide useless numbers
    - use paper color theme
    - remove sourcing to avoid double source
      - fzf install adds it to rc file

--- zsh/custom/fzf.zsh
+++ zsh/custom/fzf.zsh
@@ -1,2 +1,12 @@
-export FZF_DEFAULT_OPTS='--reverse --height 40% --color=16,bg+:magenta,gutter:-1'
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+# hide numbers
+export FZF_CTRL_R_OPTS="--with-nth 2.."
+# my style: prompt at the top
+export FZF_DEFAULT_OPTS='--reverse --height 40%'
+# paper color theme. More at https://github.com/junegunn/fzf/wiki/Color-schemes
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+    --color=fg:#4d4d4c,bg:#eeeeee,hl:#d7005f
+    --color=fg+:#4d4d4c,bg+:#e8e8e8,hl+:#d7005f
+    --color=info:#4271ae,prompt:#8959a8,pointer:#d7005f
+    --color=marker:#4271ae,spinner:#4271ae,header:#4271ae'
+
+# dont put [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh here because it will be sourced in .zshrc
