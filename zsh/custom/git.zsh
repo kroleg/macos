9531c222675fd41bcdc745607feb088d6d365d72
Vitaliy Zaytsev <teh.kroleg@gmail.com>
2025-01-23 14:07:10 +0300

    remove unused git fn alias

--- zsh/custom/git.zsh
+++
@@ -1,9 +0,0 @@
-# git fetch and rebase on default branch
-function gfrbim() {
-  git fetch
-  if git show-ref --quiet refs/heads/main; then
-    grbi origin/main
-  else
-    grbi origin/master
-  fi
-}
